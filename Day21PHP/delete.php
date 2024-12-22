e17c6dff38571214468a51df6750cfb4f363f7d9
aunghlapaing <aunghlapaing8949@gmail.com>
2024-12-22 12:49:54 +0700

    remove folder

--- Day21PHP/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once ('./dbConnection.php');
-
-$sql_query = "delete from customer where name=?";
-$data = $pdo->prepare($sql_query);
-$data->execute (['Ba Thein']);
-
-header("Location:read_prepare.php");
--- Day21PHP/insert.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('./dbConnection.php');
-
-$insert_query = "insert into customer (name, address, phone) values (?,?,?)";
-
-$insertData = $pdo->prepare ($insert_query);
-$insertData->execute(['Ba Thein', 'Mandalay', '097561234']);
-
-echo "Insert Data successful";
